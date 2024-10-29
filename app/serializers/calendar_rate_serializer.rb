bfb1cc5f4fc1df56092c74ae62ed17ad11edeeb9
sfrunza <frunza.sergiu3@gmail.com>
2024-10-28 22:31:57 -0400

    fixing

--- app/serializers/calendar_rate_serializer.rb
+++
@@ -1,6 +0,0 @@
-# class CalendarRateSerializer < ActiveModel::Serializer
-#   attributes :id, :enable_automation, :enable_auto_booking, :is_blocked, :date
-
-#   # has_one :rate, serializer: RateSerializer
-#   has_one :rate
-# end
--- app/serializers/rate_serializer.rb
+++
@@ -1,9 +0,0 @@
-# class RateSerializer < ActiveModel::Serializer
-#   attributes :id,
-#              :extra_mover_rate,
-#              :extra_truck_rate,
-#              :enable,
-#              :name,
-#              :color,
-#              :movers_rates
-# end
